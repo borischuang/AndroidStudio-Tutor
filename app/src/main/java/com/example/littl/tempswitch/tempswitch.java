1d3db9d30f0214564899a2df2e99b041b6ea75ab
borischuang <little_black27@hotmail.com>
2016-06-05 23:02:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/littl/tempswitch/tempswitch.java
@@ -0,0 +1,70 @@
+package com.example.littl.tempswitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class tempswitch extends AppCompatActivity
+    implements RadioGroup.OnCheckedChangeListener, TextWatcher
+{
+    RadioGroup rbtnGroup;
+    TextView txvType;
+    EditText edt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tempswitch);
+
+        rbtnGroup = (RadioGroup)findViewById(R.id.rbtnGroup);
+        txvType = (TextView)findViewById(R.id.txvType);
+        edt = (EditText)findViewById(R.id.edt);
+
+        rbtnGroup.setOnCheckedChangeListener(this);
+        edt.addTextChangedListener(this);
+    }
+
+    protected void selection(){
+
+        TextView txvC = (TextView)findViewById(R.id.txvC);
+        TextView txvF = (TextView)findViewById(R.id.txvF);
+
+        double f, c;
+        if(rbtnGroup.getCheckedRadioButtonId() == R.id.rbtnC){
+            f = Double.parseDouble(edt.getText().toString());
+            c = (f-32)*5/9;
+        }
+        else{
+            c = Double.parseDouble(edt.getText().toString());
+            f = c*9/5+32;
+        }
+
+        txvC.setText(String.format("%.1f", c) + getResources().getString(R.string.txvC)) ;
+        txvF.setText(String.format("%.1f", f) + getResources().getString(R.string.txvF));
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        selection();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        selection();
+    }
+}
